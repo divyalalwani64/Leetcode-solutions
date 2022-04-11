dd76041a35af83091c29cc58fa36d7d3adad3196
divyalalwani64 <97536928+divyalalwani64@users.noreply.github.com>
2022-04-11 20:50:42 +0530

    Time: 9 ms (52.71%), Space: 54.4 MB (66.28%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -3,38 +3,23 @@ class Solution {
         List<List<Integer>> result= new ArrayList();
         int rows= grid.length;
         int cols= grid[0].length; 
-        k=k%(rows*cols);
-        for(int i=0;i<k;i++)
-        {
-            int temp=grid[0][0];
-            for(int j=(rows*cols)-1;j>=0;j--)
-            {
-                int x=j/cols;
-                int y=j%cols;
-                if( x==rows-1 && y==cols-1)
-                {
-                    x=0;
-                    y=0;
-                }
-                else if(y==cols-1)
-                {
-                    x++;
-                    y=0;
-                }
-                else
-                    y++;
-                grid[x][y]=grid[j/cols][j%cols];
-                if(j==0)
-                    grid[x][y]=temp;
-            }
-        }
+        int dimensions=rows*cols;
+         k=k%(rows*cols);
+        
+      
         for(int i=0;i<rows;i++)
+            result.add(new ArrayList());// filling all the rows of result with empty lists
+        int begin=dimensions-k; // element at (begin) will be at 0,0 in new grid
+        int x=0;
+        for(int i=begin;i<begin+dimensions;i++)
         {
-            List<Integer> row= new ArrayList();
-            for(int j=0;j<cols;j++)
-                row.add(grid[i][j]);
-            result.add(new ArrayList<>(row));
+            int r=(i/cols)%rows;// calculating row and cell for new grid
+            int c=i%cols;
+            result.get(x/cols).add(grid[r][c]);// adding new element in new row
+            x++;
         }
         return result;
+// Time Complexity =O(N+N)=O(2N)=O(N)
+//Space Complexity=O(N)
     }
 }
