733bd60e79d799515218414899136f3374832a8e
divyalalwani64 <97536928+divyalalwani64@users.noreply.github.com>
2022-04-12 09:36:44 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (92.80%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -3,7 +3,7 @@ class Solution {
        int dir[][]= new int[][]{{0,1},{1,1},{1,0},{-1,1},{0,-1},{-1,-1},{-1,0},{1,-1}};//direction arrray -> possible 8 directions from the origin(0,0).
         int row=board.length;
         int col=board[0].length;
-        int res[][]=new int[row][col];// will use an extra array
+       
         for(int i=0;i<row;i++){//iterate through all rows
             for(int j=0;j<col;j++){//iterate through all cols
                 int count=0;
@@ -11,28 +11,29 @@ class Solution {
                 {
                     int p=i+d[0];
                     int q=j+d[1];
-                    if(p>=0 && p<row && q>=0 && q<col && board[p][q]==1)// check if neighbours are in range of matrix or not. and count 1.
+                    if(p>=0 && p<row && q>=0 && q<col && (board[p][q]==1 || board[p][q]==2))// check if neighbours are in range of matrix or not. and count 1 also keep track of cells which were previously alive(value=2).
                         count++;
                 }
-                //We will only use 2 conditions , because by default all the values of array will be 0, our focus to update only 1.
+                
                 if(board[i][j]==1){
-                     if(count==2 || count==3)//condition 2
-                        res[i][j]=1;
+                    //as it is already 1, we will only focus on updating 0.
+                     if(count<2 || count>3)//condition 1 && condition 2
+                        board[i][j]=2;// previous alive: now dead;
                 }
                 else{
                      if(count==3)// condition 4
-                        res[i][j]=1;
+                        board[i][j]=3;//previous dead: now alive
                 }
             }
         }
-        //Copy back to array board, because return type is void.
+        //update back 2 to 0 && 3 to 1
         for(int i=0;i<row;i++)
         {
             for(int j=0;j<col;j++)
-                board[i][j]=res[i][j];
+                board[i][j]%=2;
         }
         
         //Tc: O(n)
-        //SC:O(n)
+        //SC:O(1)
     }
 }
