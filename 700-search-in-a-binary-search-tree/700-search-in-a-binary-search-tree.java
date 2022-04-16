5b3e2019b88982e3028c3743938497142315853e
divyalalwani64 <97536928+divyalalwani64@users.noreply.github.com>
2022-04-16 15:42:24 +0530

    Time: 0 ms (100.00%), Space: 52 MB (10.17%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -14,12 +14,9 @@
  * }
  */
 class Solution {
-    TreeNode res=null;
     public TreeNode searchBST(TreeNode root, int val) {
-      if(root==null)
-            return null;
-        if(root.val==val)
+        if(root==null || root.val == val)
             return root;
-        return (root.val<val)?searchBST(root.right,val):searchBST(root.left,val);
+        return root.val<val? searchBST(root.right,val): searchBST(root.left,val);
     }
 }
