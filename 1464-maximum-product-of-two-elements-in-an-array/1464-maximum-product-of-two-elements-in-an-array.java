da0e8d88aaa6754923081730ea3a12524cc0e847
divyalalwani64 <97536928+divyalalwani64@users.noreply.github.com>
2022-04-28 13:17:28 +0530

    Time: 1 ms (89.78%), Space: 42.2 MB (75.78%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,27 +1,29 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        //3rd approach
-        int max=Integer.MIN_VALUE;
+        //also taking min value, because if we have negative numbers product will be equal to product of 2 min negative
+       int max1=Integer.MIN_VALUE;
+       int max2=Integer.MIN_VALUE;
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
+            if(nums[i]>max1)
             {
-                max=Math.max(max,(nums[i]-1)*(nums[j]-1));
+                max2=max1;
+                max1=nums[i];
+                
+            }else if(nums[i]>max2)
+                max2=nums[i];
+            if(nums[i]<min1)
+            {
+                min2=min1;
+                min1=nums[i];
             }
+            else if(nums[i]<min2)
+                min2=nums[i];
         }
-        return max;
+        return Math.max((max1-1)*(max2-1),(min1-1)*(min2-1));
     }
 }
 
-// 1st approach
-// sort the array & multiply last 2 elements.
-// Arrays.sort(nums);
-// return (nums[nums.length-1]-1) * (nums[nums.length-2]-1);
-    
-// 2nd approach
-// create max tree, multiply root & max of left & right child.
-
-//3rd approach
-//use two loops i and j , calculate (nums[i]-1)*(nums[j]-1) && find maximum.
-    
 
