912c5309705802d96c58d87a7723dd62b9160d42
divyalalwani64 <97536928+divyalalwani64@users.noreply.github.com>
2022-04-30 16:43:32 +0530

    Time: 30 ms (19.51%), Space: 52.6 MB (53.05%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,29 +1,30 @@
+//We will make use of min heap to store k elements.
 class KthLargest {
-    private static int k;
-    private PriorityQueue<Integer> heap;
-    
+    private int k;
+    //heaps are implememted using priority queue in java.
+    private PriorityQueue<Integer> pq;
     public KthLargest(int k, int[] nums) {
-        this.k = k;
-        heap = new PriorityQueue<>();
+        this.k=k;
+        pq=new PriorityQueue<>();
+        for(int n:nums)
+            pq.offer(n); 
         
-        for (int num: nums) {
-            heap.offer(num);
-        }
-        
-        while (heap.size() > k) {
-            heap.poll();
-        }
+        while(pq.size()>k)
+            pq.poll();
     }
     
     public int add(int val) {
-        heap.offer(val);
-        if (heap.size() > k) {
-            heap.poll();
-        }
-
-        return heap.peek();
+        pq.offer(val);
+        
+        if(pq.size()>k)
+            pq.poll();
+        
+        return pq.peek();
     }
 }
+//SC: O(k);
+//TC: O(n logk); logk for maintaining heap property
+
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest obj = new KthLargest(k, nums);
